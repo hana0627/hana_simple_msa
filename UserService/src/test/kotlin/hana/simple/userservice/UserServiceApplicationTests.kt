9ffdcdfe1c0f35bf82c7ca7ef4526accc90b9d8f
hana0627 <hanana9506@naver.com>
2024-10-25 11:09:27 +0900

    #1 Create UserService

---
+++ UserService/src/test/kotlin/hana/simple/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hana.simple.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

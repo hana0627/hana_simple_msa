dc83198838a5f5948915d2d8ab9acba06d7db1a1
hana0627 <hanana9506@naver.com>
2024-10-26 15:17:24 +0900

    #10 [UserService] Remove prefix ``user-service``

--- UserService/src/main/kotlin/hana/simple/userservice/api/user/controller/UserController.kt
+++ UserService/src/main/kotlin/hana/simple/userservice/api/user/controller/UserController.kt
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/user-service")
 @RequiredArgsConstructor
 class UserController (
     private val userService: UserService,
--- UserService/src/main/kotlin/hana/simple/userservice/api/user/controller/WelcomeController.kt
+++ UserService/src/main/kotlin/hana/simple/userservice/api/user/controller/WelcomeController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/user-service")
 class WelcomeController {
     @GetMapping("/")
     fun welcome(): String {

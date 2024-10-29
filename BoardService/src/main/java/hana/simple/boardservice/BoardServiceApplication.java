5bf6bbc67c8c4191b93aa438af4832f1099835d8
hana0627 <hanana9506@naver.com>
2024-10-29 14:57:19 +0900

    #19 Setting BoardService

--- BoardService/src/main/java/hana/simple/boardservice/BoardServiceApplication.java
+++ BoardService/src/main/java/hana/simple/boardservice/BoardServiceApplication.java
@@ -2,8 +2,10 @@ package hana.simple.boardservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BoardServiceApplication {
 
     public static void main(String[] args) {
